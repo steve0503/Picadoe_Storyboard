d57be3074b28d9f7a08ce7f5e349ba2ad85fc27a
steve0503 <leehr76@gmail.com>
2014-01-16 07:58:55 +0900

    Picadeo_Storyboard
    Leehaeyong

---
+++ CameraTest/MainViewController.h
@@ -0,0 +1,13 @@
+//
+//  MainViewController.h
+//  CameraTest
+//
+//  Created by SDT-1 on 2014. 1. 14..
+//  Copyright (c) 2014년 iamdreamer. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MainViewController : UIViewController
+
+@end
---
+++ CameraTest/MainViewController.m
@@ -0,0 +1,48 @@
+//
+//  MainViewController.m
+//  CameraTest
+//
+//  Created by SDT-1 on 2014. 1. 14..
+//  Copyright (c) 2014년 iamdreamer. All rights reserved.
+//
+
+#import "MainViewController.h"
+
+@interface MainViewController ()
+
+@property (weak, nonatomic) IBOutlet UICollectionView *homeScreen;
+
+@end
+
+@implementation MainViewController
+
+
+- (IBAction)searchButton:(id)sender {
+    
+    
+    
+    
+}
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
